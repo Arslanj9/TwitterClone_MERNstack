800b21f69127dc4f0e792c2c846e001208bdeb80
Arslan Javaid <aja118379@gmail.com>
2024-09-23 08:16:23 +0500

    Connected MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
@@ -10,9 +11,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser());
 
 
-app.use("/api/auth", authRoutes);
+// app.use("/api/auth", authRoutes);
 
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB()
 });
